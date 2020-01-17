10f5f27354eb947a54d01f59d4a5486329c391d1
Adam Haywood <adambhaywood@gmail.com>
2020-01-17 03:40:06 -0500

    added t shirt design and color update
    design and color fields now work as intended

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 // variables to be called throughout project
 const $userName = $('#name');
 const $jobRole = $('#title');
-const $otherField = $('#other-title')
-const $shirtDesign = $('#design')
+const $otherField = $('#other-title');
+const $shirtDesign = $('#design');
+const $color = $('#color');
+let $colorOptions = $('#color option')
+const $selectThemeOption = $('#design option:nth-child(1)');
 
 // initial focus of name input
 $userName.focus();
@@ -17,11 +20,31 @@ $jobRole.on('change', function() {
   } else $otherField.hide();
 });
 
-
-console.log($shirtDesign.val());
-
-
-
-
+// hiding color options and telling user to pick design option first
+$colorOptions.hide();
+$color.prepend('<option value="standard">Please select a T-shirt theme</option>')
+$("option[value='standard']").attr('selected','selected')
 
 
+//hides the select theme option in the options but is still shown when loading page
+$selectThemeOption.hide();
+
+// this function hides all shirt color options when the specified theme is chosen
+// also will hide all previous colors when the theme is modified
+$shirtDesign.on('change', function() {
+  $colorOptions = $('#color option');
+  if ($shirtDesign.val() === 'js puns') {
+    $colorOptions.hide().removeAttr('selected');
+    $('#color option:nth-child(2)').show().attr('selected', 'selected');
+    $('#color option:nth-child(3)').show();
+    $('#color option:nth-child(4)').show();
+  } else if ($shirtDesign.val() === 'heart js') {
+    $colorOptions.hide().removeAttr('selected');
+    $('#color option:nth-child(5)').show().attr('selected', 'selected');
+    $('#color option:nth-child(6)').show();
+    $('#color option:nth-child(7)').show();
+  } else {
+    $colorOptions.hide().removeAttr('selected');
+    $('#color option:nth-child(1)').show().attr('selected', 'selected');
+  }
+})
